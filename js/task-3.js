aa1150361a3737f3066bd98e9b7b8a325a20495c
Olga <olakosmolove17@gmail.com>
2026-08-20 17:03:36 +0300

    Fix task 3 according to feedback

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,7 @@
 function getElementWidth(content, padding, border) {
-    const contentValue = parseFloat(content);
-    const paddingValue = parseFloat(padding);
-    const borderValue = parseFloat(border);
-
-    return contentValue + paddingValue * 2 + borderValue * 2;
+  return content + padding * 2 + border * 2;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth(50, 8, 4)); // 74
+console.log(getElementWidth(60, 12, 8.5)); // 101
+console.log(getElementWidth(200, 0, 0)); // 200
